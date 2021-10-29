eb27f076534ba33c22e08b3f3cb7f9ea41063a26
MaylaBeatriz <maylaetb@gmail.com>
2021-10-29 10:53:52 -0300

    separando as funcionalidades em arquivos diferentes

---
+++ introducao-javascript-inicial/introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,41 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido";
+        // paciente.style.color = "red";
+        // paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!")
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaValida && pesoValido) {
+        var imc = peso / (altura * altura);   //100 / 2.00 * 2.00 = 100 / 4 = 25
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
