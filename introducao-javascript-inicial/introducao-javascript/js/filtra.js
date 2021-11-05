db06845aa2685e9251c89f37c871455cf485ace8
MaylaBeatriz <maylaetb@gmail.com>
2021-11-05 11:43:47 -0300

    pegando os nomes dos pacientes para filtrar

--- introducao-javascript-inicial/introducao-javascript/js/filtra.js
+++ introducao-javascript-inicial/introducao-javascript/js/filtra.js
@@ -1,4 +1,8 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function(){
-    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i < pacientes.length; i++) {
+        var paciente = pacientes[i];
+        var nome = paciente.querySelector(".info-nome").textContent;
+    }
 })
