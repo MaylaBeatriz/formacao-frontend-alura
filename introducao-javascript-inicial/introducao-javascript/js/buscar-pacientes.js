bdbdcdaca3452826147e48d90134fb1b2bc17f0d
MaylaBeatriz <maylaetb@gmail.com>
2021-11-09 15:48:31 -0300

    adicionando os pacientes JSON na tabela

--- introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
@@ -5,11 +5,10 @@ botaoBuscar.addEventListener("click", function() {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function() {
         var resposta = xhr.responseText;
-        console.log(resposta);
-        console.log(typeof resposta);
         var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
-        console.log(typeof pacientes);
+        pacientes.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
     });
     xhr.send();
 });
