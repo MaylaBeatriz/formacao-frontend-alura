7ed60f946e4f77e97f640250ff2df7e5868bd2c8
MaylaBeatriz <maylaetb@gmail.com>
2021-11-09 15:46:32 -0300

    criando função adicionaPacienteNaTabela

--- introducao-javascript-inicial/introducao-javascript/js/form.js
+++ introducao-javascript-inicial/introducao-javascript/js/form.js
@@ -5,7 +5,6 @@ botaoadicionar.addEventListener("click", function(event) {
     
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);    
-    var pacienteTr = montaTr(paciente);
     var erros = validaPaciente(paciente);
 
     console.log(erros);
@@ -15,13 +14,18 @@ botaoadicionar.addEventListener("click", function(event) {
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensdeErro(erros) {
     var ul = document.querySelector("#mensagens-erro");
