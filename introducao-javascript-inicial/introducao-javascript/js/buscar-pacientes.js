67794062128872df6534170fd68bd722673a32ec
MaylaBeatriz <maylaetb@gmail.com>
2021-11-09 15:56:11 -0300

    capturando erros de requisição

--- introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
@@ -2,13 +2,18 @@ var botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/paciente");
     xhr.addEventListener("load", function() {
+        if (xhr.status == 200) {
         var resposta = xhr.responseText;
         var pacientes = JSON.parse(resposta);
         pacientes.forEach(function(paciente) {
             adicionaPacienteNaTabela(paciente);
         });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
     });
     xhr.send();
 });
