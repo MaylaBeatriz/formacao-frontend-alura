48a325b52e267f72dabd483fe72f1134dd4e9ca3
MaylaBeatriz <maylaetb@gmail.com>
2021-11-12 14:40:22 -0300

    adicionando mensagem de erro

--- introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
@@ -2,17 +2,21 @@ var botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/paciente");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientess");
     xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
         if (xhr.status == 200) {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(function(paciente) {
+            console.log(paciente);
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function(paciente) {
             adicionaPacienteNaTabela(paciente);
         });
         }else{
             console.log(xhr.status);
             console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
         }
     });
     xhr.send();
